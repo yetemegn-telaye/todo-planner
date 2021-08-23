d58e5e0945e02465ef683b6b7d641c4d8db6a054
yetemegn-telaye <75949338+yetemegn-telaye@users.noreply.github.com>
2021-08-23 18:36:47 +0300

    Adding Dummy Data
    Added todo and category lists in shared folder

---
+++ shared/categories.js
@@ -0,0 +1,26 @@
+export const CATEGORIES = [
+    {
+        id: 0,
+        name: 'Chores',
+        description: 'Write your day to day chores, and get them done',
+        createdDate: "2020-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 1,
+        name: 'Projects',
+        description: 'Organize and plan your projects',
+        createdDate: "2020-12-16T17:57:28.556094Z"
+    },
+    {
+        id: 2,
+        name: 'Reading Books',
+        description: 'Track Your Book reading',
+        createdDate: "2020-12-16T17:57:28.556094Z"
+    },
+    {
+        id: 3,
+        name: 'Groccery Shopping',
+        description: 'organize shopping list',
+        createdDate: "2020-08-16T17:57:28.556094Z"
+    }
+]
---
+++ shared/todos.js
@@ -0,0 +1,82 @@
+export const TODOS= [
+    {
+        id: 0,
+        categoryId: 0,
+        name: 'Clean House',
+        description: 'cleaning the livingroom and kitchen',
+        createdDate: "2020-10-16T17:57:28.556094Z",
+        dueDate: "2020-10-16T17:57:28.556094Z",
+        completed: true,
+        hrs: 3
+    },
+    {
+        id: 1,
+        categoryId: 0,
+        name: 'Cook Food',
+        description: 'Cook the weeks food',
+        createdDate: "2020-11-16T17:57:28.556094Z",
+        dueDate: "2020-11-16T17:57:28.556094Z",
+        completed: false,
+        hrs: 5
+    },
+    {
+        id: 2,
+        categoryId: 1,
+        name: 'Todo App Project',
+        description: 'making a todo app for github profile',
+        createdDate: "2020-11-16T17:57:28.556094Z",
+        dueDate: "2020-11-16T17:57:28.556094Z",
+        completed: true,
+        hrs: 6
+    },
+    {
+        id: 3,
+        categoryId: 1,
+        name: 'MuleSoft Project',
+        description: 'try out mulesoft project',
+        createdDate: "2020-12-16T17:57:28.556094Z",
+        dueDate: "2020-12-16T17:57:28.556094Z",
+        completed: false,
+        hrs: 3
+    },
+    {
+        id: 4,
+        categoryId: 2,
+        name: 'Alchemist',
+        description: 'finish chapter 3',
+        createdDate: "2020-08-16T17:57:28.556094Z",
+        dueDate: "2020-08-16T17:57:28.556094Z",
+        completed: false,
+        hrs: 1
+    },
+    {
+        id: 5,
+        categoryId: 2,
+        name: 'The Street Lawyer',
+        description: 'finish the first chapter',
+        createdDate: "2020-09-16T17:57:28.556094Z",
+        dueDate: "2020-09-16T17:57:28.556094Z",
+        completed: false,
+        hrs: 1
+    },
+    {
+        id: 6,
+        categoryId: 3,
+        name: 'Fruits',
+        description: 'get fruits strawberry, mango, banana',
+        createdDate: "2020-08-16T17:57:28.556094Z",
+        dueDate: "2020-08-16T17:57:28.556094Z",
+        completed: false,
+        hrs: 1
+    },
+    {
+        id: 7,
+        categoryId: 3,
+        name: 'Meat',
+        description: 'dekak meat',
+        createdDate: "2020-08-16T17:57:28.556094Z",
+        dueDate: "2020-08-16T17:57:28.556094Z",
+        completed:true,
+        hrs: 1
+    }
+]
